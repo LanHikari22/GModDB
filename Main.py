b900941b674731b6013349d6c8c46fda017fc5fc
Lan <lanhikarixx@gmail.com>
2017-06-03 02:33:16 -0500

    Starting to work on the console

--- Main.py
+++ Main.py
@@ -1,10 +1,21 @@
 import sys
-# sys.path.insert(0, '/home/lan/Projects/XML_Parser/')
+sys.path.insert(0, '/home/lan/Projects/XML_Parser/')
+sys.path.insert(0, '/home/lan/Projects/XML_Writer/')
+
 from XMLWriter import *
 from AbstractXMLWriterHandler import *
 from XMLParser import *
 from AbstractXMLParserHandler import *
 from XMLTag import XMLTag
+from Console import Console
+
+console = Console()
+
+def processArgs():
+    pass
 
 if __name__ == '__main__':
-    print("I'm back!!!!")
+
+    if len(sys.argv) > 1:
+        processArgs()
+
